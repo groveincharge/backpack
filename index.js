3c5cdc6add13f1fafe8b6bc83dd50a05371ce5c3
groveincharge <wdasent@yahoo.com>
2023-04-03 19:44:29 -0400

    fixing routes for production.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://bossdetail.onrender.com",
   credentials: true
 }));
 
